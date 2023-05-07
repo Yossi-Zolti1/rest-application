6f1e3cb560ced44072ec6e050f96dc0da8e0eb62
yossi <yberman8@gmail.com>
2023-05-07 12:09:57 +0300

    add validation for reset password

--- server/controllers/users/UsersValidations.js
+++ server/controllers/users/UsersValidations.js
@@ -32,7 +32,11 @@ class UsersValidations {
   
   static validResetPassword(body) {
     let userSchema = joi.object({
-      newPassword: joi.string().trim().min(6),
+      password: joi.string().trim().min(9)
+        .regex(/[A-Z]/) // at least one uppercase letter
+        .regex(/[a-z]/) // at least one lowercase letter
+        .regex(/[0-9]/) // at least one number
+        .required(),
     });
     return userSchema.validate(body)
   }
