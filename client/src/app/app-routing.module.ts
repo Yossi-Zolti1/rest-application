f517acc6020a7afb6673fb68fb2db2a0badf5377
Yossi-Zolti <yzolti@gmail.com>
2023-05-18 11:27:26 +0300

    guards

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,19 +5,19 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { RegisterOwnerComponent } from './components/admin/registerOwner/register-owner.component';
-import { AuthGuardService } from './services/auth-guard.service';
+import { AuthAdminGuardService } from './services/auth-admin-guard.service';
 import { ResetPasswordComponent } from './pages/resetPassword/reset-password.component';
 import { ManagerPageComponent } from './pages/manager/managerPage/manager-page.component';
-import { AuthGuardManager } from './services/auth-guard-manager';
+import { AuthOwnerGuardService } from './services/auth-owner-guard.service';
 import { RestaurantDetailsComponent } from './pages/manager/restaurant-details/restaurant-details.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'admin', component: RegisterOwnerComponent, canActivate: [AuthGuardService] },
+  { path: 'admin', component: RegisterOwnerComponent, canActivate: [AuthAdminGuardService] },
   { path: 'resetPassword/:id/:token', component: ResetPasswordComponent},
-  { path: 'owner', component: ManagerPageComponent, canActivate: [AuthGuardManager] },
+  { path: 'owner', component: ManagerPageComponent, canActivate: [AuthOwnerGuardService] },
   { path: 'restaurant-details', component: RestaurantDetailsComponent },
 ];
 
